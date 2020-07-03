5933da5de3d89cb18310d63c388a626b428f66bd
Tutj <tuantuluong@gmail.com>
2020-07-03 18:06:05 +0800

    Change name of edge

--- components/iot-frontend/src/app/app-routing.module.ts
+++ components/iot-frontend/src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
-    path: 'sensors',
+    path: 'edge1',
     loadChildren: () => import('./sensors/sensors.module').then( m => m.SensorsPageModule)
   },
   {
